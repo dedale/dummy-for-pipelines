aeb42ef60b792755a004b0ceb390a8ed3ea1d3d5
dedale <ded@laposte.net>
2020-10-17 21:57:50 +0200

    [Dummy] Add tests

--- src/Applicative/Highway/Tests/Tests.cs
+++ src/Applicative/Highway/Tests/Tests.cs
@@ -1,18 +1,39 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Highway.UnitTests
 {
-    [TestFixture, Property("Perimeter", "Highway")]
+    [Property("Perimeter", "Highway")]
+    [TestFixtureSource(nameof(values))]
     public class Tests
     {
+        static int[] values = Enumerable.Range(0, 500).ToArray();
+        private readonly double duration;
+        public Tests(int value)
+        {
+            duration = Math.Round(Math.Exp(5.0 * value / 1000), 3);
+        }
         [Test] public void Test()
         {
             Assert.AreEqual("Highway.Code", typeof(Code).FullName);
+            Console.WriteLine($"Would wait {duration} s");
+        }
+    }
+
+    [TestFixture]
+    public class Sandbox
+    {
+        [Test, Explicit] public void Test()
+        {
+            var fixtures = 1000;
+            var xMin = 0.0;
+            var xMax = 6.0;
+            var duration = Enumerable.Range(0, 1000)
+                .Select(i => Math.Exp(xMin + (xMax - xMin) * i / fixtures))
+                .Sum();
+            Console.WriteLine(duration);
         }
     }
 }
--- src/Marvel/Maths/Tests/Tests.cs
+++ src/Marvel/Maths/Tests/Tests.cs
@@ -1,14 +1,24 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Linq;
 
 namespace Marvel.Maths.UnitTests
 {
-    [TestFixture, Property("Perimeter", "Core")]
+    [Property("Perimeter", "Core")]
+    [TestFixtureSource(nameof(values))]
     public class Tests
     {
-        [Test] public void Test()
+        static int[] values = Enumerable.Range(0, 500).ToArray();
+        private readonly double duration;
+        public Tests(int value)
+        {
+            duration = Math.Round(Math.Exp(5.0 * value / 1000), 3);
+        }
+        [Test]
+        public void Test()
         {
             Assert.AreEqual("Marvel.Maths.Code", typeof(Code).FullName);
+            Console.WriteLine($"Would wait {duration} s");
         }
     }
 }
